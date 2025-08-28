35a3cfa4f610fcafe40d6fdc42bc3fa76c979941
rammhapasekar <mhapasekar.ram@gmail.com>
2025-08-28 23:50:29 +0100

    Initial Commit

---
+++ CurrencyExchange/CurrencyExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyExchangeApp.swift
+//  CurrencyExchange
+//
+//  Created by Ram Mhapasekar on 28/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
